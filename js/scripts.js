7909716ccd76ff581ac0640eeefb5bc7e109eb72
Mathew Gitata <gitataIT@gmail.com>
2019-09-04 16:59:45 +0300

    Add constants for google books api, search button and the display area.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// api, search button, display area constants
+const apikey = "AIzaSyDdkUJrGPPVWMRg-enDhs2Y_2xvkvQT6lI";
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const displayArea = document.getElementById("result");
