cbe25282c531729fc6cbdd2ad1b85000c913728a
Mathew Gitata <gitataIT@gmail.com>
2019-09-05 09:42:58 +0300

    Validate user input as well as start google books fetch process

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,29 @@ const displayArea = document.getElementById("result");
 
 // start of search button event listener
   searchButton.addEventListener("click", searchBook);
-  document.addEventListener("keypress", function(query){
+  document.addEventListener("keypress", function(searchEntry){
     if (event.keyCode == 13) {
       console.log("You pressed enter key");
-      searchBook(query);
+      searchBook(searchEntry);
     }
   });
 // end of search button event listener
 
 // start function to validate user entry and initializa fetchingprocess
-function searchBook(query){
-  
+function searchBook(searchEntry){
+  searchEntry.preventDefault();
+  if(searchInput.value != ""){
+    const rawInput = searchInput.value.toLowerCase();
+    const query = rawInput.replace(/\s/,"+");
+    asyncCall(query);
+  } else {
+    alert("Please type a search to begin book find.");
+  }
 }
-// start function to validate user entry and initializa fetchingprocess
+// end function to validate user entry and initializa fetchingprocess
+
+// start function to make api calls to google api
+function asyncCall(query){
+
+}
+// end function to make api calls to google api
