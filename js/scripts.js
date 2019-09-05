aadbb4cc5c185af7d84c28f8f9f65895a7396ac6
Mathew Gitata <gitataIT@gmail.com>
2019-09-05 08:46:11 +0300

    Add event listeners to capture enter keypress and initialize google books fetching process

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ const apikey = "AIzaSyDdkUJrGPPVWMRg-enDhs2Y_2xvkvQT6lI";
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const displayArea = document.getElementById("result");
+
+// start of search button event listener
+  searchButton.addEventListener("click", searchBook);
+  document.addEventListener("keypress", function(query){
+    if (event.keyCode == 13) {
+      console.log("You pressed enter key");
+      searchBook(query);
+    }
+  });
+// end of search button event listener
+
+// start function to validate user entry and initializa fetchingprocess
+function searchBook(query){
+  
+}
+// start function to validate user entry and initializa fetchingprocess
